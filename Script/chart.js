d95f4daeb5549ee84e2154957b376ccdfc20c297
LocNguyen1994 <pin-da-naus@hotmail.com>
2018-11-14 20:49:39 +0100

    click function uitegebreid

--- Script/chart.js
+++ Script/chart.js
@@ -1,11 +1,15 @@
-d3.json('log2.json').then(function(data){
+d3.json('log.json').then(function(data){
   console.log(data)
 const svg = d3.select("body").append("svg").attr("width", "100%").attr("height","100%")
 const colors = d3.scaleOrdinal(d3.schemePastel1);
 const height = 300;
 const width = 600;
 const margin = {top: 50, bottom: 50, left: 50, right: 50}
-console.log(data)
+//  TodoLIst
+// Shit functional maken er is heel veel code dubbelop zoals de barchart declaren!!
+
+
+
 // D3 data manupilatie variables
 let nestedData = d3.nest()// met nest kijk je naar platte data en return je het als nested object.
       .key(function(d) { return d.taal; })
@@ -17,7 +21,7 @@ console.log(nestedData)
 const test = d3.nest()
                 .key(function(d){return d.values.title})
                 .entries(nestedData)
-console.log(test)
+
 // Pie declaratie's en het opzetten van de pie
 let pieData = d3.pie().sort(null).value(function(d,i){return d.values.length})(nestedData)
 
@@ -36,10 +40,12 @@ const sections = svg.append("g")
 
 sections.enter()
         .append("path")
+        .attr("class","sections")
         .attr("fill", function(d,i){return colors(d.data.values.length)})
         .attr("d", segments)
         .on("mouseover", handleMouseOver)
         .on("mouseout", handleMouseOut)
+        .on("click", handleClick)
 
 let text = svg.select("g").selectAll("text").data(pieData)
 text.enter().append("text").each(function(d){
@@ -70,6 +76,7 @@ let xAxis = d3.axisBottom(x)
 chartGroup.selectAll("rect")
         .data(nestedData)
         .enter().append("rect")
+                    .attr("class", "chartRect")
                     .attr("width", x.bandwidth())
                     .attr("height", function(d,i){return height- y(d.values.length);})
                     .attr("x", function(d){return x(d.key)})
@@ -103,7 +110,7 @@ const scale  = 1;
 function handleMouseOver(d, i){
   // CODEVOORBEELD van stackoverflow voorbeeld: Linkje staat in de README
   // Link stackoverflow: https://stackoverflow.com/questions/47581324/can-an-array-be-used-as-a-d3-nest-key
-  console.log(d)
+
   // let currentSectionText = svg.select(".currentSectionText").data(d)
   // currentSectionText.enter()
   //                     .append("g")
@@ -117,40 +124,8 @@ function handleMouseOver(d, i){
                       .text(function(){return `Taal: ${capatalize(d.data.key)}, Aantal Boeken: ${d.data.values.length}  `})
                       .attr("fill",function(){return colors(d.data.values.length)})
 
-  // svg.append("text")
-  //     .data(d)
-  //     .attr("class", "currentSection")
-  //     .text(function(d){return d.data.key})
-  //     .attr("transform","translate(100,350)")
-
-
-
-// let text = svg.select("g").selectAll("text").data(pieData)
-// text.enter().append("text").each(function(d){
-//   let center = segments.centroid(d);
-//   d3.select(this)
-//       .attr("x", center[0])
-//       .attr("y", center[1])
-//       .text(d.data.values.length)
-// })
-
   // Implementatie van de codevoorbeeld
-  let arrayGenreSeparated =[]
-  d.data.values.forEach(function(book){
-    book.genre.forEach(function(single_genre){
-      arrayGenreSeparated.push({
-        titel: book.title,
-        taal: book.taal,
-        genre: single_genre,
-      })
-    })
-  })
-  // console.log(currentData)
-  let genre = d3.nest()
-        .key(function(d) { return d.genre})
-        .entries(arrayGenreSeparated)
-  // console.log(currentData)
-  console.log(genre)
+
   // console.log(d.data.values.map(function(d){d.genre}) })
   d3.select(this)
       .transition()
@@ -168,12 +143,108 @@ function handleMouseOut(d, i){
       .attr("transform", `scale(${scale})`)
 }
 
-function capatalize(string){
-  return string.toUpperCase()
+function handleClick(d){
+  const current = this
+  let currentSegment = d3.arc()
+                        .innerRadius(180)
+                        .outerRadius(250)
+                        .padAngle(0.2)
+                        .padRadius(10)
+
+  let newSegment = d3.arc()
+                        .innerRadius(200)
+                        .outerRadius(250)
+                        .padAngle(0.2)
+                        .padRadius(10)
+
+  const donut = d3.select(".donut.chart")
+                    .transition()
+                    .duration(500)
+                    .attr("transform", "translate(750,500)")
+  donut.selectAll("path")
+        .transition()
+        .duration(500)
+        .attr("d", newSegment)
+        .style("opacity", function(d){return (this === current) ? 1 : .3})
+        // .attr("transform", `scale(${changeSize(current)})`)
+        .attr("transform", function(d){return (this === current) ? "scale(1.3)" : "scale(1.2)"})
+
+
+  donut.selectAll("text")
+        .transition()
+        .duration(200)
+        .attr("opacity", 0)
+
+  let arrayGenreSeparated =[]
+  d.data.values.forEach(function(book){
+    book.genre.forEach(function(single_genre){
+      arrayGenreSeparated.push({
+        titel: book.title,
+        taal: book.taal,
+        genre: single_genre,
+      })
+    })
+  })
+
+  let genre = d3.nest()
+  .key(function(d) { return d.genre})
+  .entries(arrayGenreSeparated)
+
+// let y= d3.scaleLinear()
+//             .domain([0, d3.max(nestedData,function(d){return d.values.length})])
+//             .range([height, 0])
+// let x= d3.scaleBand()
+//             .domain(nestedData.map(function(d){return d.key}))
+//             .range([0, barWidth])
+//             .paddingInner(0.15)
+  let genreCount = genre.map(function(d){return d.key})
+  console.log(genreCount)
+  x.domain(genreCount).range([0, xAsLength(genreCount.length)])
+  y.domain([0, d3.max(genre, function(d){return d.values.length})])
+
+  svg.select(".axis.x")
+      .transition()
+      .duration(750)
+      .call(xAxis)
+  svg.select(".axis.y")
+      .transition()
+      .duration(750)
+      .call(yAxis)
+  // svg.selectAll(".chartRect")
+  //     .data(genre)
+  //     .enter()
+  //     .attr("width", x.bandwidth())
+  //     .attr("height", function(d,i){return height- y(d.values.length);})
+  //     .attr("x", function(d){return x(d.key)})
+  //     .attr("fill", function(d,i){return colors(d.values.length)})
+  //     .attr("y", function(d,i){return y(d.values.length);})
+
+
+
+
+
+
+
+
+  // Disable Hover function in click state
+  d3.selectAll(".sections").on("mouseover",null)
+  .on("mouseout",null)
 }
 
 
+function xAsLength(genres){
+  console.log(genres)
+  if(genres > 10){
+    return 1200
+  }else{
+    return 800
+  }
+}
 
+
+function capatalize(string){
+  return string.toUpperCase()
+}
 
 
 
